371fb5280ba407c9bb57c734ef39254b479f9c26
Степан <s_kislin@mail.ru>
2026-06-02 16:57:05 +0500

    Update db.php

--- src/config/databases/db.php
+++ src/config/databases/db.php
@@ -1,12 +1,14 @@
 <?php
-
+// config data
 $host = 'localhost';
 $db = 'Working';
 $user = 'root';
 $pass = 'root';
 
+// mysqli connect function
 $conn = new mysqli($host, $user, $pass, $db);
 
+// try connect
 if ($conn->connect_error) {
     die("Ошибка подключения: " . $conn->connect_error);
 }
